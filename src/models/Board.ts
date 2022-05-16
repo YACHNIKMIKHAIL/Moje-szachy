6d00aab941ad30418bb90b3911cb611da09b1ae9
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-05-16 13:18:56 +0200

    something  broked commit

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,4 +1,5 @@
 import {Cell} from "./Cell";
+import {Colors} from "./Colors";
 
 export class Board {
     cells: Cell[][] = []
@@ -7,12 +8,13 @@ export class Board {
         for (let i = 0; i < 8; i++) {
             const row: Cell[] = []
             for (let j = 0; i < 8; j++) {
-                if (i + j % 2 !== 0) {
-                    row.push(new Cell())//black
+                if ((i + j) % 2 !== 0) {
+                    row.push(new Cell(this, j, i, Colors.BLACK, null))//black
                 } else {
-                    row.push(new Cell())//white
+                    row.push(new Cell(this, j, i, Colors.WHITE, null))//white
                 }
             }
+            this.cells.push(row)
         }
     }
 }
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,2 +1,23 @@
-export class Cell{
+import {Colors} from "./Colors";
+import {Figure} from "./figures/Figure";
+import {Board} from "./Board";
+
+export class Cell {
+    readonly x: number
+    readonly y: number
+    readonly color: Colors
+    figure: Figure | null
+    board: Board
+    availeble: boolean //if you can do
+    id: number //for REACT keys
+
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+        this.x = x
+        this.y = y
+        this.color = color
+        this.figure = figure
+        this.board = board
+        this.availeble = false
+        this.id = Math.random()
+    }
 }

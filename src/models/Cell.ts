dfd7c9462d39979cc4430531228210994a150194
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-05-16 18:34:13 +0200

    isEmptyDiagonal samaia gest! commit

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -40,10 +40,34 @@ export class Cell {
     }
 
     isEmptyHorizontal(target: Cell): boolean {
+        if (this.y !== target.y) {
+            return false
+        }
+        const min = Math.min(this.x, target.x)
+        const max = Math.max(this.x, target.x)
+        for (let x = min + 1; x < max; x++) {
+            if (!this.board.getCell(x, this.y).isEmpty()) {
+                return false
+            }
+        }
         return true
     }
 
     isEmptyDiagonal(target: Cell): boolean {
+        const absX = Math.abs(target.x - this.x)
+        const absY = Math.abs(target.y - this.y)
+        if (absX !== absY) {
+            return false
+        }
+
+        const dy = this.y < target.y ? 1 : -1
+        const dx = this.x < target.x ? 1 : -1
+
+        for (let i = 1; i < absY; i++) {
+            if (!this.board.getCell(this.x + dx * i, this.y + dy * i)) {
+                return false
+            }
+        }
         return true
     }
 

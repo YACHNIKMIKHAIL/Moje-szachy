fc06e9c6f7fe0350fb7f98b03575ca9c8d846ed9
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-05-16 17:03:00 +0200

    all figures staying in each cell commit

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -3,6 +3,9 @@ import {Colors} from "./Colors";
 import {Queen} from "./figures/Queen";
 import {Pawn} from "./figures/Pawn";
 import {Bishop} from "./figures/Bishop";
+import {King} from "./figures/King";
+import {Rook} from "./figures/Rook";
+import {Knight} from "./figures/Knight";
 
 export class Board {
     cells: Cell[][] = []
@@ -26,15 +29,22 @@ export class Board {
     }
 
     private addBishops() {
-
+        new Bishop(Colors.WHITE, this.getCell(2, 0))
+        new Bishop(Colors.WHITE, this.getCell(5, 0))
+        new Bishop(Colors.BLACK, this.getCell(2, 7))
+        new Bishop(Colors.BLACK, this.getCell(5, 7))
     }
 
     private addKings() {
-
+        new King(Colors.WHITE, this.getCell(4, 0))
+        new King(Colors.BLACK, this.getCell(4, 7))
     }
 
     private addKnights() {
-
+        new Knight(Colors.WHITE, this.getCell(1, 0))
+        new Knight(Colors.WHITE, this.getCell(6, 0))
+        new Knight(Colors.BLACK, this.getCell(1, 7))
+        new Knight(Colors.BLACK, this.getCell(6, 7))
     }
 
     private addPawns() {
@@ -45,11 +55,15 @@ export class Board {
     }
 
     private addQueens() {
-
+        new Queen(Colors.WHITE, this.getCell(3, 0))
+        new Queen(Colors.BLACK, this.getCell(3, 7))
     }
 
-    private addPooks() {
-
+    private addRooks() {
+        new Rook(Colors.WHITE, this.getCell(0, 0))
+        new Rook(Colors.WHITE, this.getCell(7, 0))
+        new Rook(Colors.BLACK, this.getCell(0, 7))
+        new Rook(Colors.BLACK, this.getCell(7, 7))
     }
 
     public addFigures() {
@@ -58,6 +72,6 @@ export class Board {
         this.addKnights()
         this.addPawns()
         this.addQueens()
-        this.addPooks()
+        this.addRooks()
     }
 }

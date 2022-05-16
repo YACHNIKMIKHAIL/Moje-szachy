767d88912b2d4d6012d32fcf05ef395638fb572f
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-05-16 12:35:56 +0200

    class public initCells commit

---
+++ src/models/Board.ts
@@ -0,0 +1,18 @@
+import {Cell} from "./Cell";
+
+export class Board {
+    cells: Cell[][] = []
+
+    public initCells() {
+        for (let i = 0; i < 8; i++) {
+            const row: Cell[] = []
+            for (let j = 0; i < 8; j++) {
+                if (i + j % 2 !== 0) {
+                    row.push(new Cell())//black
+                } else {
+                    row.push(new Cell())//white
+                }
+            }
+        }
+    }
+}
